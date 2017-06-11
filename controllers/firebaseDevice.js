71d9c879d21a9fd0f80b1d15c8c271c77b66a178
Alexis_Ni <alexntitoras@hotmail.gr>
2017-06-11 18:38:14 +0300

    device token database

--- controllers/firebaseDevice.js
+++ controllers/firebaseDevice.js
@@ -14,18 +14,41 @@ exports.saveTokenDevice = function (req, res, next) {
         status: '500'
       })
     }
-  DeviceToken.findOne({token: registrationToken})
-    .exec(function (err, result) {
-      if(err){
-        return res.status(500).json({
-          where: 'firebaseDevice',
-          title: 'An error occured ',
-          err:err
-        })
-      }
-      if (result) {
-        DeviceToken.update({_id: result._id}, {$set: {userId: userId}})
-          .exec(function (err, result) {
+    DeviceToken.findOne({token: registrationToken})
+      .exec(function (err, result) {
+        if (err) {
+          return res.status(500).json({
+            where: 'firebaseDevice',
+            title: 'An error occured ',
+            err: err
+          })
+        }
+        if (result) {
+          DeviceToken.update({_id: result._id}, {$set: {userId: userId}})
+            .exec(function (err, result) {
+              if (err) {
+                return res.status(500).json({
+                  title: 'Error',
+                  message: 'An error has occured....',
+                  status: '500',
+                  err: err
+                })
+
+              }
+              return res.status(201).json({
+                message: 'Device Token Updated',
+                obj: result
+              })
+
+            })
+
+        } else {
+          var devToken = new DeviceToken({
+            userId: userId,
+            token: registrationToken
+          })
+
+          devToken.save(function (err, result) {
             if (err) {
               return res.status(500).json({
                 title: 'Error',
@@ -33,40 +56,39 @@ exports.saveTokenDevice = function (req, res, next) {
                 status: '500',
                 err: err
               })
-
             }
             return res.status(201).json({
-              message: 'Device Token Updated',
+              message: 'Device Token saved',
               obj: result
-            })
+            });
+            /*        User.findOne({_id: userId})
+             .exec(function (err, user) {
+             if (err) {
+             return res.status(500).json({
+             title: 'Error',
+             message: 'An error has occured....',
+             status: '500',
+             err: err
+             })
+             }
 
+             user.deviceToken.push(result)
+             user.save(function (err, userSaved) {
+             if (err) {
+             return res.status(500).json({
+             title: 'Error',
+             message: 'An error has occured....',
+             status: '500',
+             err: err
+             })
+             }
+
+             })
+             })*/
           })
-
-      } else {
-        var devToken = new DeviceToken({
-          userId: user,
-          token: registrationToken
-        })
-
-        devToken.save(function (err, result) {
-          if (err) {
-            return res.status(500).json({
-              title: 'Error',
-              message: 'An error has occured....',
-              status: '500',
-              err: err
-            })
-          }
-          return res.status(201).json({
-            message: 'Device Token saved',
-            obj: userSaved
-          });
-
-        })
-      }
-    });
-  });
-
+        }
+      })
+  })
 
 
 }
