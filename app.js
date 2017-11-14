8a0b55216d6c1338faa72faf2d9623c9c9380cf0
Alexis_Ni <alexntitoras@hotmail.gr>
2017-11-14 17:49:35 +0200

    chanches

--- app.js
+++ app.js
@@ -58,6 +58,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 
 /*
 app.use(function (req, res, next) {
